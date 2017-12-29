f3372fedb4e9725c1047506a9bb6f212ef897cde
jwmartin0988 <jwmartin0988@gmail.com>
2017-12-29 04:34:08 -0600

    fdsaf
    asdfas

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ if __name__ == "__main__":
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
+    print("Server Started...")
     server.serve_forever()
